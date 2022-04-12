2b1a300ee4374b047b2dcf37153191869b5c2b3e
Arpit Surana <suranaarpit34@gmail.com>
2022-04-12 10:12:38 +0530

    Day 1 Assign. uploaded

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import Card from "./Day 1/one/card";
 import Imp from "./Day 1/two/Imp";
+import Imp2 from "./Day 1/two/Imp2";
 
 
 function App() {
   return (
     <>
     {/* <Card ></Card> */}
-    <Imp></Imp>
+    {/* <Imp></Imp> */}
+    <Imp2></Imp2>
     </>
   
   );
